7a166d40f1676a94ff2eee0c2dd3820e63528b5f
jhon rodriguez <jhonrodri2605@gmail.com>
2023-10-21 08:15:48 -0500

    Creacion del deb context para la entidad Mark

---
+++ Context/Entities/Mark.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Context.Entities
+{
+    public class Mark
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool State { get; set; }
+
+    }
+}
